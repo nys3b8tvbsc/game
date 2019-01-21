6e42fc70ad1d0e6d95681ba01b95cb3942d3f8c2
KrashR13 <KrashR26@gmail.com>
2019-01-22 00:48:56 +0300

    Панель персонажа

--- panel.py
+++ panel.py
@@ -9,41 +9,41 @@ class Panel:
     TODO
     """
 
-    def __init__(self, screen, players_config):
+    def __init__(self, screen_width):
         """
         :param screen: pygame.Surface
         :param players_config: Dict
         :return: Panel object
         """
-        self.screen = screen
-        self.size = screen.get_width(), 50
+        self.width = screen_width
         self.pos = 0, 0
-        self.color = (128, 64, 64)
-        self.main_surface = pygame.Surface(self.size)
-        self.main_surface.fill(self.color)
+        self.main_surface = pygame.image.load('pictures/panel.png').convert_alpha()
+        self.height=int(self.main_surface.get_height()*self.width/self.main_surface.get_width())
+        self.main_surface=pygame.transform.scale(self.main_surface,(self.width,self.height))
+        self.rect=self.main_surface.get_rect()
         self.font = pygame.font.Font(None, 20)
         self.text_color = (0, 0, 0)
+
+    def blit_me(self,surface,player_config):
+        """
+        TODO
+        """
         self.text11 = self.font.render('HP', 0, self.text_color)
         self.text12 = self.font.render('25/100', 0, self.text_color)
         self.text21 = self.font.render('Mana', 0, self.text_color)
         self.text22 = self.font.render('50/100', 0, self.text_color)
-
-    def blit_me(self):
-        """
-        TODO
-        """
         self.main_surface.blit(self.text11, (10, 10))
         self.main_surface.blit(self.text21, (10, 30))
         self.main_surface.blit(self.text12, (70, 10))
         self.main_surface.blit(self.text22, (70, 30))
-        self.screen.blit(self.main_surface, self.pos)
+        surface.blit(self.main_surface, self.pos)
 
 
-"""
+
 pygame.init()
 screen = pygame.display.set_mode()
-i = Panel(screen, {})
-i.blit_me()
+i = Panel(screen.get_width())
+i.blit_me(screen,{})
 pygame.display.update()
 clock = pygame.time.Clock()
 param = True
@@ -53,4 +53,4 @@ while param:
             param = False
     pygame.display.update()
     clock.tick(60)
-"""
+
