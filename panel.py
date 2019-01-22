5026f889aea9c1f4265c8dac076b8d511e64d9d6
nys3b8tvbsc <34589158+nys3b8tvbsc@users.noreply.github.com>
2019-01-22 03:19:15 +0300

    Style changes.

--- panel.py
+++ panel.py
@@ -3,6 +3,8 @@ Module provides top panel.
 """
 import pygame
 
+from constans import BLACK
+
 
 class Panel:
     """
@@ -10,36 +12,41 @@ class Panel:
     """
 
     def __init__(self, screen_width):
-
         """
-        :param screen: pygame.Surface
-        :param players_config: Dict
+        :param screen_width: int
         :return: Panel object
         """
         self.width = screen_width
         self.pos = 0, 0
         self.main_surface = pygame.image.load('pictures/panel.png').convert_alpha()
-        self.height=int(self.main_surface.get_height()*self.width/self.main_surface.get_width())
-        self.main_surface=pygame.transform.scale(self.main_surface,(self.width,self.height))
-        self.rect=self.main_surface.get_rect()
+        self.height = int(self.main_surface.get_height() * self.width / self.main_surface.get_width())
+        self.main_surface = pygame.transform.scale(self.main_surface, (self.width, self.height))
+        self.rect = self.main_surface.get_rect()
         self.font = pygame.font.Font(None, 20)
-        self.text_color = (0, 0, 0)
+        self.text_color = BLACK
 
-    def blit_me(self,surface,hero):
+    def get_rect(self):
+        """
+        :return: pygame.Rect object
+        """
+        return self.rect
+
+    def blit_me(self, surface, hero):
         """
         TODO
         """
-        self.hp = '{}/{}'.format(hero['hp'], hero['max_hp'])
-        self.mana = '{}/{}'.format(hero['mana'], hero['max_mana'])
-        self.text11 = self.font.render('HP', 0, self.text_color)
-        self.text12 = self.font.render(self.hp, 0, self.text_color)
-        self.text21 = self.font.render('Mana', 0, self.text_color)
-        self.text22 = self.font.render(self.mana, 0, self.text_color)
-        self.main_surface.blit(self.text11, (10, 10))
-        self.main_surface.blit(self.text21, (10, 30))
-        self.main_surface.blit(self.text12, (70, 10))
-        self.main_surface.blit(self.text22, (70, 30))
+        hp = '{}/{}'.format(hero['hp'], hero['max_hp'])
+        mana = '{}/{}'.format(hero['mana'], hero['max_mana'])  # TODO refactor energy
+        t_hp = self.font.render('HP', 0, self.text_color)
+        i_hp = self.font.render(hp, 0, self.text_color)
+        t_mana = self.font.render('Mana', 0, self.text_color)
+        i_mana = self.font.render(mana, 0, self.text_color)
+        self.main_surface.blit(t_hp, (10, 10))
+        self.main_surface.blit(t_mana, (10, 30))
+        self.main_surface.blit(i_hp, (70, 10))
+        self.main_surface.blit(i_mana, (70, 30))
         surface.blit(self.main_surface, self.pos)
+
 
 """
     def update(self, hero):
@@ -50,7 +57,7 @@ class Panel:
 pygame.init()
 screen = pygame.display.set_mode()
 i = Panel(screen.get_width())
-i.blit_me(screen,{'hp':50,'max_hp':100,'mana':25,'max_mana':100})
+i.blit_me(screen, {'hp': 50, 'max_hp': 100, 'mana': 25, 'max_mana': 100})
 pygame.display.update()
 clock = pygame.time.Clock()
 param = True
@@ -60,4 +67,3 @@ while param:
             param = False
     pygame.display.update()
     clock.tick(60)
-
