73e476490b429796dc049563e044855d5d4e8dc9
KrashR13 <KrashR26@gmail.com>
2019-01-23 20:40:22 +0300

    print_txt() version 1.0 ;)

--- button.py
+++ button.py
@@ -1,5 +1,5 @@
 import pygame
-
+from constans import BLACK, WHITE
 
 class Button(pygame.sprite.Sprite):
     def __init__(self, pos, size, text, on_click, color):
@@ -17,3 +17,32 @@ class Button(pygame.sprite.Sprite):
 
     def blit_me(self):
         pass
+
+
+def print_txt(surface,rect,text, font, font_size,color): #version 1.0 :)
+    f=pygame.font.Font(font,font_size)
+    n=rect.width//f.get_linesize()
+    while len(text)>n:
+        num=text[:n].rfind(' ')+1
+        if num<=0:
+            num=n
+        txt=f.render(text[:num],1,color)
+        surface.blit(txt,rect)
+        text=text[num:]
+        rect.y+=f.get_height()+2
+    txt = f.render(text, 1, color)
+    surface.blit(txt, rect)
+
+"""
+pygame.init()
+srf=pygame.display.set_mode()
+srf.fill(WHITE)
+rect=pygame.Rect((0,0,500,100))
+print_txt(srf,rect,'Какой-то непонятный текст, который нужно отобразить в ректе','fonts/DECOR6DI.TTF',30,BLACK)
+pygame.display.update()
+param=True
+while param:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            param = False
+"""
