93afb81592491f3951403d56e706a0a7d80f2d76
nys3b8tvbsc <34589158+nys3b8tvbsc@users.noreply.github.com>
2019-01-23 03:52:48 +0300

    Add label

--- button.py
+++ button.py
@@ -1,19 +1,28 @@
 import pygame
 
 
-class Button(pygame.sprite.Sprite):
-    def __init__(self, pos, size, text, on_click, color):
+class Button():
+    def __init__(self, pos, size, label, on_click, color):
         """
-
         :param pos: Tuple(x, y)
         :param size: Tuple(width, height)
-        :param text: str
+        :param label: Label object
         :param on_click: Callable
         :param color: Tuple(R, G, B)
         """
         pygame.sprite.Sprite().__init__(self)
-        self.rect = pygame.Rect(pos, size)
-        self.rect.fill(color)
+        self.surface = pygame.Surface(size)
+        self.surface.fill(color)
+        self.rect = (pos, size)
+        self.label = label
+        self.label.set_rect(center=(size[0] / 2, size[1] / 2))
 
-    def blit_me(self):
-        pass
+        self.on_click = on_click
+
+    def blit_me(self, screen):
+        self.label.blit_me(self.surface)
+        screen.blit(self.surface, self.rect)
+
+
+def create_button(config):
+    pass
---
+++ label.py
@@ -0,0 +1,14 @@
+import pygame
+
+from constans import BLACK, WHITE
+
+
+class Label:
+    def __init__(self, size, text, font, font_size, color=WHITE, text_color=BLACK):
+        self.surface = pygame.Surface(size)
+        self.rect = self.surface.get_rect()
+        self.font = pygame.font.Font(font, font_size)
+        self.text = self.font.render(text, 0, text_color)
+
+    def set_rect(self, center):
+        self.rect = self.surface.get_rect(center)
