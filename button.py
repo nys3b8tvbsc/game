3b1174ef29319c36d80a669e3a636f684b14bfec
nys3b8tvbsc <34589158+nys3b8tvbsc@users.noreply.github.com>
2019-01-25 04:21:40 +0300

    Add main fields to button.

--- button.py
+++ button.py
@@ -1,37 +1,35 @@
 import pygame
 
+from label import Label
+
 
 class Button:
-    def __init__(self, pos, label, on_click):
+    def __init__(self, size=(0, 0), pos=(0, 0), on_press=lambda: None, label=Label()):
         """
+        :param size: Tuple(width, height)
         :param pos: Tuple(x, y)
+        :param on_press: Callable
         :param label: Label object
-        :param on_click: Callable
+        :return: Button object
         """
-        self.label = label
-        self.label.rect[0] = pos
-        self.rect = self.label.rect
 
-        self.on_click = on_click
+        self.__image = pygame.image.load('Рома доделай и подгони по размеру').convert_alpha()  # TODO
 
-    def blit_me(self, screen):
-        self.label.blit_me(self.screen)
+        self.__rect = self.__image.get_rect()
+        self.__rect.move_ip(pos)
+
+        self.__label = label
+
+        self.on_press = on_press
+
+    def blit_me(self, surface):
+        self.__label.blit_me(self.__image)
+        surface.blit(self.__image, self.__rect)
+
+    @property
+    def rect(self):
+        return self.__rect
 
 
-def create_button(config):
+def create_button(config, creator, size=(0, 0), pos=(0, 0)):
     pass
-
-
-def print_txt(surface, rect, text, font, font_size, color):  # version 1.0 :)
-    f = pygame.font.Font(font, font_size)
-    n = rect.width // f.get_linesize()
-    while len(text) > n:
-        num = text[:n].rfind(' ') + 1
-        if num <= 0:
-            num = n
-        txt = f.render(text[:num], 1, color)
-        surface.blit(txt, rect)
-        text = text[num:]
-        rect.y += f.get_height() + 2
-    txt = f.render(text, 1, color)
-    surface.blit(txt, rect)
