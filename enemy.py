e61f6eb71dc774fc350c7944b8de569e52e63b87
KrashR13 <KrashR26@gmail.com>
2019-01-25 13:00:08 +0300

    Исправление в enemy

--- enemy.py
+++ enemy.py
@@ -9,7 +9,7 @@ class Enemy(Unit):
     """Base class for all enemies.
     TODO implement base class methods"""
 
-    def __init__(self, power, max_hp, level):
+    def __init__(self, level, power, max_hp, animations):
         """
         :param level: int
         :param power: int
@@ -19,8 +19,28 @@ class Enemy(Unit):
         Unit.__init__(self, max_hp, level)
         self.power = power
         self.bar = pygame.image.load('pictures/manabar.png').convert_alpha()
+        self.animations=animations
+        self.state = APPEAR
+        self.image = self.animations[APPEAR].frame
+        self.rect = self.image.get_rect()
+        self.bar_rect = self.bar.get_rect(center=self.rect.center)
         # self.actions = actions
 
+    def animated(self):
+        if not self.animations[self.state].update():
+            self.state = DEFAULT
+        self.image = self.animations[self.state].frame
+
+    def blit_me(self, surface):
+        """
+        :param surface: pygame.Surface
+        """
+        surface.blit(self.image, self.rect)
+        self.bar_rect.y = self.rect.y - 25
+        pygame.draw.rect(surface, RED, (
+            self.bar_rect.x + X_BAR, self.bar_rect.y + Y_BAR, int(MAX_HP_BAR * (float(self.hp / self.max_hp))), H_BAR))
+        surface.blit(self.bar, self.bar_rect)
+        
     def make_action(self, hero):
         """
         :param hero: Hero
@@ -43,34 +63,16 @@ class Golem(Enemy):
         :param power: int
         :param max_hp: int
         """
-        Enemy.__init__(self, level, power, max_hp)
         self.animations = []
         self.animations.append(Animation('pictures/Archive (1)/idle-walk/idle', 6))
         self.animations.append(Animation('pictures/Archive (1)/attack/hit', 6))
         self.animations.append(Animation('pictures/Archive (1)/appear/appear', 15))
         self.animations.append(Animation('pictures/Archive (1)/die/die', 7))
-        self.state = APPEAR
-        self.image = self.animations[APPEAR].frame
-        self.rect = self.image.get_rect()
-        self.bar_rect = self.bar.get_rect(center=self.rect.center)
-
-    def animated(self):
-        if not self.animations[self.state].update():
-            self.state = DEFAULT
-        self.image = self.animations[self.state].frame
-
-    def blit_me(self, surface):
-        """
-        :param surface: pygame.Surface
-        """
-        surface.blit(self.image, self.rect)
-        self.bar_rect.y = self.rect.y - 25
-        pygame.draw.rect(surface, (255, 0, 0), (
-            self.bar_rect.x + X_BAR, self.bar_rect.y + Y_BAR, int(MAX_HP_BAR * (float(self.hp / self.max_hp))), H_BAR))
-        surface.blit(self.bar, self.bar_rect)
+        Enemy.__init__(self, level, power, max_hp, self.animations)
 
 
-"""
+
+
 pygame.init()
 srf = pygame.display.set_mode((1000, 500))
 gol = Golem(1, 1000, 1000)
@@ -88,4 +90,4 @@ while param:
     gol.blit_me(srf)
     pygame.display.update()
     clock.tick(20)
-"""
+
