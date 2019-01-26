f1e97a6971a3e91f83707e35dd61ea3eff8e8000
nys3b8tvbsc <34589158+nys3b8tvbsc@users.noreply.github.com>
2019-01-26 03:57:16 +0300

    Update scene.

--- scene.py
+++ scene.py
@@ -1,6 +1,4 @@
-"""
-TODO
-"""
+import os
 from abc import ABCMeta, abstractmethod
 
 import pygame
@@ -9,19 +7,23 @@ import pygame
 class Scene(metaclass=ABCMeta):
     def __init__(self, image, screen_size):
         width = screen_size[0]
-        self.image = pygame.image.load(image).convert_alpha()
-        height = int(self.image.get_height() * width / self.image.get_width())
-        self.image = pygame.transform.scale(self.image, (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.move_ip(0, screen_size[1] - height)
+        self._image = pygame.image.load(image).convert_alpha()
+        height = int(self._image.get_height() * width / self._image.get_width())
+        self._image = pygame.transform.scale(self._image, (width, height))
+        self._rect = self._image.get_rect()
+        self._rect.move_ip(0, screen_size[1] - height)
+
+    @property
+    def rect(self):
+        return self._rect
 
     @abstractmethod
     def update(self):
         pass
 
     @abstractmethod
-    def blit_me(self, screen):
-        screen.blit(self.image, self.rect)
+    def blit_me(self, surface):
+        surface.blit(self._image, self._rect)
 
     @abstractmethod
     def handle_event(self, event):
@@ -30,46 +32,40 @@ class Scene(metaclass=ABCMeta):
     @property
     @abstractmethod
     def is_over(self):
-        pass
-
-    @abstractmethod
-    def get_data(self):
-        """
-        :return: Dict
-        """
         pass
 
 
 class Battle(Scene):
     def __init__(self, screen_size, scene_config):
-        Scene.__init__(self, scene_config['image'], screen_size)
+        self._image_path = os.path.join('BG', scene_config['image'])
+        Scene.__init__(self, self._image_path, screen_size)
 
     def update(self):
         pass
 
-    def blit_me(self, screen):
-        Scene.blit_me(self, screen)
+    def blit_me(self, surface):
+        Scene.blit_me(self, surface)
 
     def handle_event(self, event):
         pass
 
     @property
     def is_over(self):
-        pass
-
-    def get_data(self):
         pass
 
 
 class Quest(Scene):
     def __init__(self, screen_size, scene_config):
-        Scene.__init__(self, scene_config['image'], screen_size)
+        self._image_path = os.path.join('pictures', 'blueframe2.png')
+        Scene.__init__(self, self._image_path, screen_size)
 
     def update(self):
         pass
 
-    def blit_me(self, screen):
-        Scene.blit_me(self, screen)
+    def blit_me(self, surface):
+        for button in self._buttons:
+            button.blit_me(surface)
+        Scene.blit_me(self, surface)
 
     def handle_event(self, event):
         pass
@@ -78,14 +74,11 @@ class Quest(Scene):
     def is_over(self):
         pass
 
-    def get_data(self):
-        pass
-
 
 def create_scene(screen_size, scene_config):
-    """
-    :param screen_size: Tuple(width, height)
-    :param scene_config: Dict
-    :return: Union[Battle, Quest] object
-    """
-    pass
+    if scene_config['type'] == 'quest':
+        pass
+    elif scene_config['type'] == 'battle':
+        pass
+    else:
+        raise ValueError('Scene has unknown type.')
