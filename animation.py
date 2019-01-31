aa7349e2867fe6a44167c0ded2e904fa19cc2f91
nys3b8tvbsc <nys3b8tvbsc@yandex.ru>
2019-01-31 14:06:12 +0300

    Refactor animation.

--- animation.py
+++ animation.py
@@ -1,21 +1,26 @@
 """
 Module provides Animation class for units animations.
 """
+import os
 from itertools import cycle
 
 import pygame
 
 
+def count_files(path):
+    return len([0 for obj in os.scandir(path) if obj.is_file()])
+
+
 class Animation:
-    def __init__(self, animation_path, cycle_len):
+    def __init__(self, animation_path):
         """
         :param animation_path: str
-        :param cycle_len: int
-        :return: Animation object
+        :rtype: Animation object
         """
+        cycle_len = count_files(animation_path)
         frames = []
         for i in range(cycle_len):
-            frame_path = animation_path + '_{}.png'.format(i + 1)
+            frame_path = animation_path + '_{}.png'.format(i)
             frame = pygame.image.load(frame_path).convert_alpha()
             frames.append(frame)
 
