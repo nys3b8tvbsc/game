d1939802a1ad09032c25aa9b23d96e44ac295a3c
nys3b8tvbsc <nys3b8tvbsc@yandex.ru>
2019-02-05 03:07:24 +0300

    Update Panel / Game.

--- game.py
+++ game.py
@@ -60,21 +60,19 @@ class Game:
                 if event.key == pygame.K_F4 and event.mod == pygame.KMOD_LALT:
                     pygame.quit()
                     sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # Left click
-                    if self._scene.rect.collidepoint(event.pos):
-                        self._scene.handle_event(event)
-                    elif self._panel.rect.collidepoint(event.pos):
-                        self._panel.handle_event(event)
+            else:
+                self._scene.handle_event(event)
+                self._panel.handle_event(event)
 
     def game_logic(self):
-        self._scene.update(self._hero)
+        self._scene.update()  # TODO
         self._panel.update(self._hero)
         # TODO ??
 
     def screen_blit(self):
         self._scene.blit_me(self._screen)
         self._panel.blit_me(self._screen)
+        pygame.display.update()
         if self._scene.is_over:
             self._scene.get_data()  # TODO refactor ??
             # TODO if data ...
